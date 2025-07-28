808fe63745a8a783ca72f977b610e6b6851f6434
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-07-28 10:53:11 -0500

    feat: Add more styling concerns to example project

--- HTML-CSS/structured-form/javascript.js
+++ HTML-CSS/structured-form/javascript.js
@@ -12,3 +12,21 @@ const colorPicker = document.querySelector("#color");
 colorPicker.addEventListener("input", ()=>{
     document.body.style.backgroundColor = colorPicker.value;
 })
+
+document.addEventListener(
+    "DOMContentLoaded",
+    ()=> {
+        document
+        .getElementById("billing-checkbox")
+        .addEventListener("change", toggleBilling);
+    },
+    false
+);
+
+function toggleBilling() {
+    const billingItems = document.querySelectorAll('#billing input[type="text"');
+
+    for(const item of billingItems) {
+        item.disabled = !item.disabled;
+    }
+}
