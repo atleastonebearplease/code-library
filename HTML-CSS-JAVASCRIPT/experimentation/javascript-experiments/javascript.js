47009fe3e06dbddf5e297eea7f541cef8d71a791
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-08-24 09:31:19 -0500

    feat: Add object protoype stuff

--- HTML-CSS-JAVASCRIPT/experimentation/javascript-experiments/javascript.js
+++ HTML-CSS-JAVASCRIPT/experimentation/javascript-experiments/javascript.js
@@ -24,7 +24,32 @@ function Player(name, marker) {
     }
 }
 
-const player1 = Player('steve', 'X');
+const player1 = new Player('steve', 'X');
 const player2 = new Player ('also steve', 'O');
 player1.sayName();
 player2.sayName();
+
+function Book(title, author, pageCount, hasBeenRead) {
+    if(!new.target){
+        throw Error("You must use the 'new' operator to call the constructor")
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.hasBeenRead = hasBeenRead;
+
+    this.info = function() {
+        let readString = '';
+
+        if(hasBeenRead === true) readString = 'has been read';
+        else readString = 'has not been read';
+
+        return `${title} by ${author}, ${pageCount} pages, ${readString}`;
+    }
+}
+
+let newBook = new Book("The Hobbit", "J.R.R. Tolkien", 120, true);
+
+console.log(newBook.info());
+console.log(Object.getPrototypeOf(player1) === Player.prototype);
